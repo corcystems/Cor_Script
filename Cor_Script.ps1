82e0d23c52668c02ad67805dee79e0cedccbe88d
corMHauser <158617946+corMHauser@users.noreply.github.com>
2024-09-10 09:30:13 -0400

    Update Cor_Script.ps1

--- Cor_Script.ps1
+++ Cor_Script.ps1
@@ -3,7 +3,7 @@
 
 ## Get Hash
 $wc = [System.Net.WebClient]::new()
-$pkgurl = 'https://raw.githubusercontent.com/corcystems/Corcystems-Public/master/CorTools/Cor_Script.ps1'
+$pkgurl = 'https://raw.githubusercontent.com/corcystems/Cor_Script/main/Cor_Script.ps1'
 $localHash = Get-FileHash C:\Cor\Cor_Script.ps1 -Algorithm SHA256
 $FileHash = Get-FileHash -InputStream ($wc.OpenRead($pkgurl)) -Algorithm SHA256
 $FileHash.Hash -eq $localHash.Hash
