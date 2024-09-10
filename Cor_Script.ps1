376063c8d173424b8d8c968bdabfd93623622adc
corMHauser <158617946+corMHauser@users.noreply.github.com>
2024-09-10 09:45:55 -0400

    Update Cor_Script.ps1

--- Cor_Script.ps1
+++ Cor_Script.ps1
@@ -1,9 +1,79 @@
 ## Created by Mike Hauser 
 ## Version 0.1
+## Help Section
 
+param([switch] $repair )
+if ($repair)
+{
+    write-host "This is help for this program. It does nothing. Hope that helps."
+}
+
+
+### Variables ###
+$scriptURL = 'https://raw.githubusercontent.com/corcystems/Cor_Script/main/Cor_Script.ps1'
+$scriptPath = 'C:\Cor\Cor_Script.ps1'
+
+
+
+### Functions ###
 ## Get Hash
 $wc = [System.Net.WebClient]::new()
-$pkgurl = 'https://raw.githubusercontent.com/corcystems/Cor_Script/main/Cor_Script.ps1'
-$localHash = Get-FileHash C:\Cor\Cor_Script.ps1 -Algorithm SHA256
-$FileHash = Get-FileHash -InputStream ($wc.OpenRead($pkgurl)) -Algorithm SHA256
-$FileHash.Hash -eq $localHash.Hash
+$localHash = Get-FileHash $scriptPath -Algorithm SHA256
+$fileHash = Get-FileHash -InputStream ($wc.OpenRead(scriptURL)) -Algorithm SHA256
+$fileHash.Hash -eq $localHash.Hash
+
+## Update Script
+
+
+
+## Show sript version
+
+
+
+### CorCystem Tools ###
+## CW Automate
+# CWA Uninstall
+
+# CWA Install
+
+## CW ScreenConnect
+# CWSC Uninstall
+
+# CWSC Install
+
+## CW ScreenConnect Hosted
+# CWSCH Uninstall
+
+# CWSCH Install
+
+## Immybot
+# Immybot Uninstall
+
+# Immybot Install
+
+## SentinelOne
+# SentinelOne Uninstall
+
+# SentinelOne Install
+
+
+
+
+### Troubleshooting ###
+## OS Cleanups
+# SFC
+
+# DISM
+
+# Optimize
+
+# Repair
+
+# reset and clear print spooler
+
+## Informational
+# DSREGCMD /STATUS (for domain / azure join info)
+
+# grab public ip
+
+# speedtest
